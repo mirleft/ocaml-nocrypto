4e6311ec00065bb3f343119f38206a8647230901
pqwy <unitedbiscuits@gmail.com>
2014-02-24 00:13:14 +0000

    ARC4

--- src/nocrypto.ml
+++ src/nocrypto.ml
@@ -55,7 +55,7 @@ struct
   (* 64 should be enough for everybody! *)
   let blocksize = 64
 
-  let lpad0 cs =
+  let rpad0 cs =
     let open Cstruct in
     let l   = len cs
     and cs' = create blocksize in
@@ -68,18 +68,67 @@ struct
     for i = 0 to len - 1 do Cstruct.set_uint8 cs i x done;
     cs
 
-  let hmac_opad = create_with blocksize 0x5c
-  let hmac_ipad = create_with blocksize 0x36
+  let opad = create_with blocksize 0x5c
+  let ipad = create_with blocksize 0x36
 
   let of_hash_fn hash key message =
-    let key = if Cstruct.len key > blocksize then hash key  else key in
-    let key = if Cstruct.len key < blocksize then lpad0 key else key in
-    let opad = cs_xor key hmac_opad
-    and ipad = cs_xor key hmac_ipad in
-    hash (opad <> hash (ipad <> message))
+    let key = if Cstruct.len key > blocksize then hash key else key in
+    let key = if Cstruct.len key < blocksize then rpad0 key else key in
+    let outer = cs_xor key opad
+    and inner = cs_xor key ipad in
+    hash (outer <> hash (inner <> message))
 
   let sha1 ~key cs = of_hash_fn Hash.sha1 key cs
   let md5  ~key cs = of_hash_fn Hash.md5  key cs
+
 end
 
+module ARC4 : sig
+  open Cstruct
+  type key
+  val of_secret : t -> key
+  val encrypt : key -> t -> key * t
+  val decrypt : key -> t -> key * t
+end
+  =
+struct
+
+  type key = int * int * int array
+
+  let of_secret cs =
+    let len = Cstruct.len cs in
+    let s   = Array.init 256 (fun x -> x) in
+    let rec loop j = function
+      | 256 -> ()
+      | i ->
+          let x = Cstruct.get_uint8 cs (i mod len) in
+          let si = s.(i) in
+          let j = (j + si + x) land 0xff in
+          let sj = s.(j) in
+          s.(i) <- sj ; s.(j) <- si ;
+          loop j (succ i)
+    in
+    ( loop 0 0 ; (0, 0, s) )
+
+  let encrypt (i, j, s') cs =
+    let s   = Array.copy s'
+    and len = Cstruct.len cs in
+    let res = Cstruct.create len in
+    let rec mix i j = function
+      | n when n = len -> (i, j, s)
+      | n ->
+          let i  = succ i land 0xff in
+          let si = s.(i) in
+          let j  = (j + si) land 0xff in
+          let sj = s.(j) in
+          let k  = s.((si + sj) land 0xff) in
+          s.(i) <- sj ; s.(j) <- si ;
+          Cstruct.(set_uint8 res n (k lxor get_uint8 cs n));
+          mix i j (succ n)
+    in
+    (mix i j 0, res)
+
+  let decrypt = encrypt
+
+end
 
