9ce4cf44e03ac0a4f2ac1aba4c3cbb78eabdce51
pqwy <unitedbiscuits@gmail.com>
2014-02-26 01:42:30 +0000

    add aes to standalone

--- tests/runnable.ml
+++ tests/runnable.ml
@@ -33,6 +33,7 @@ let () =
 
 *)
 
+let cs_of_s = Cstruct.of_string
 
 let on_stdin fn =
   lwt input = Lwt_io.(read stdin) in
@@ -41,19 +42,38 @@ let on_stdin fn =
   Cstruct.hexdump_to_buffer b res;
   Lwt_io.printl (Buffer.contents b)
 
+let aes_key str = AES.of_secret (Cstruct.of_string str)
+
 let main () =
-  try
-    match Sys.argv.(1) with
-    | "md5"       -> on_stdin Hash.md5
-    | "sha1"      -> on_stdin Hash.sha1
-    | "hmac_md5"  -> on_stdin (Hmac.md5  ~key:(Cstruct.of_string Sys.argv.(2)))
-    | "hmac_sha1" -> on_stdin (Hmac.sha1 ~key:(Cstruct.of_string Sys.argv.(2)))
-    | _           -> invalid_arg "args"
-  with Invalid_argument _ ->
-    Printf.eprintf
-      "%s: [ md5 | sha1 | hmac_md5 <key> | hmac_sha1 <key> ]\n%!"
-      Sys.argv.(0);
-    exit 1
+  match List.tl (Array.to_list Sys.argv) with
+
+  | ["md5"]       -> on_stdin Hash.md5
+
+  | ["sha1"]      -> on_stdin Hash.sha1
+
+  | ["hmac_md5" ; k] -> on_stdin @@ Hmac.md5  ~key:(cs_of_s k)
+
+  | ["hmac_sha1"; k] -> on_stdin @@ Hmac.sha1 ~key:(cs_of_s k)
+
+  | ["aes"; "encrypt"; k] -> on_stdin AES.(encrypt @@ of_secret (cs_of_s k))
+
+  | ["aes"; "decrypt"; k] -> on_stdin AES.(decrypt @@ of_secret (cs_of_s k))
+
+  | ["aes_ecb"; "encrypt"; k] -> on_stdin AES.(encrypt_ecb @@ aes_key k)
+
+  | ["aes_ecb"; "decrypt"; k] -> on_stdin AES.(decrypt_ecb @@ aes_key k)
+
+  | ["aes_cbc"; "encrypt"; k; iv] -> on_stdin @@ fun i ->
+      snd AES.(encrypt_cbc (aes_key k) (cs_of_s iv) i)
+
+  | ["aes_cbc"; "decrypt"; k; iv] -> on_stdin @@ fun i ->
+      snd AES.(decrypt_cbc (aes_key k) (cs_of_s iv) i)
+
+  | _ ->
+      Printf.eprintf
+        "%s: [ md5 | sha1 | hmac_md5 <key> | hmac_sha1 <key> ]\n%!"
+        Sys.argv.(0);
+      exit 1
 
 let () = Lwt_main.run @@ main ()
 
