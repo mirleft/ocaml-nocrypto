9c85c2af48296640362759db24806c6963036604
pqwy <unitedbiscuits@gmail.com>
2014-03-04 05:16:45 +0100

    factor some cstruct tools out

--- src/cstruct_.ml
+++ src/cstruct_.ml
@@ -22,6 +22,16 @@ let concat css =
       0 css in
   result
 
+let copy cs =
+  let len = Cstruct.len cs in
+  let cs' = Cstruct.create len in
+  ( Cstruct.blit cs 0 cs' 0 len ; cs' )
+
+let xor_into src dst =
+  for i = 0 to Cstruct.len dst - 1 do
+    Cstruct.(set_uint8 dst i (get_uint8 src i lxor get_uint8 dst i))
+  done
+
 let xor cs1 cs2 =
   let len = Cstruct.(min (len cs1) (len cs2)) in
   let cs = Cstruct.create len in
@@ -30,4 +40,58 @@ let xor cs1 cs2 =
   done;
   cs
 
+let fill cs x =
+  for i = 0 to Cstruct.len cs - 1 do
+    Cstruct.set_uint8 cs i x
+  done
+
+let create_with n x =
+  let cs = Cstruct.create n in ( fill cs x ; cs )
+
+let rpad cs size x =
+  let open Cstruct in
+  let l   = len cs in
+  let cs' = create size in
+  assert (size >= l) ;
+  blit cs 0 cs' 0 l ;
+  for i = l to size - 1 do set_uint8 cs' i x done ;
+  cs'
+
+let lpad cs size x =
+  let open Cstruct in
+  let l   = len cs in
+  let cs' = create size
+  and i0  = size - l in
+  assert (size >= l) ;
+  blit cs 0 cs' i0 l ;
+  for i = 0 to i0 - 1 do set_uint8 cs' i x done ;
+  cs'
+
+let of_bytes, of_int32s, of_int64s =
+  let aux k set xs =
+    let cs = Cstruct.create (List.length xs * k) in
+    List.iteri (fun i x -> set cs (i * k) x) xs;
+    cs
+  in
+  (aux 1 set_uint8, aux 4 BE.set_uint32, aux 8 BE.set_uint64)
+
+let empty = Cstruct.create 0
+
 let (<>) = append
+
+let of_hex str =
+
+  let digit c =
+    let open Char in
+    let i = code (lowercase c) in
+    if i >= code '0' && i <= code '9' then i - code '0' else
+    if i >= code 'a' && i <= code 'f' then i - code 'a' + 10
+    else invalid_arg "hex." in
+
+  let len = String.length str / 2 in
+  let cs  = Cstruct.create len in
+  for i = 0 to len - 1 do
+    Cstruct.set_uint8 cs i @@
+      (hex str.[i * 2] lsl 4) + hex str.[i * 2 + 1]
+  done;
+  cs
--- src/hash.ml
+++ src/hash.ml
@@ -9,26 +9,13 @@ module Build ( H : Hash_fn ) = struct
 
   include H
 
-  let rpad0 cs =
-    let open Cstruct in
-    let l   = len cs
-    and cs' = create hmac_block in
-    blit cs 0 cs' 0 l;
-    for i = l to hmac_block - 1 do set_uint8 cs' i 0 done;
-    cs'
-
-  let create_with len x =
-    let cs = Cstruct.create len in
-    for i = 0 to len - 1 do Cstruct.set_uint8 cs i x done;
-    cs
-
-  let opad = create_with hmac_block 0x5c
-  let ipad = create_with hmac_block 0x36
+  let opad = Cstruct_.create_with hmac_block 0x5c
+  let ipad = Cstruct_.create_with hmac_block 0x36
 
   let hmac ~key message =
     let open Cstruct_ in
     let key = if len key > hmac_block then digest key else key in
-    let key = if len key < hmac_block then rpad0 key else key in
+    let key = if len key < hmac_block then rpad key hmac_block 0 else key in
     let outer = xor key opad
     and inner = xor key ipad in
     digest (outer <> digest (inner <> message))
