63ce4d97d10a1592caef68110a510eeccd73c65b
pqwy <unitedbiscuits@gmail.com>
2014-03-15 18:47:34 +0100

    make rsa's use of rng patently obvious

--- src/rsa.ml
+++ src/rsa.ml
@@ -34,57 +34,60 @@ let decrypt_unsafe ~key: ({ p; q; dp; dq; q' } : priv) c =
   Z.(m2 + h * q)
 
 (* Timing attacks, you say? *)
-let decrypt_blinded_unsafe ~key: ({ e; n } as key : priv) c =
+let decrypt_blinded_unsafe ?g ~key: ({ e; n } as key : priv) c =
 
   let rec nonce () =
-    let x = Rng.Z.gen_r Z.(of_int 2) n in
+    let x = Rng.Z.gen_r ?g Z.(of_int 2) n in
     if Z.(gcd x n = one) then x else nonce () in
 
   let r  = nonce () in
+  Printf.printf "blind: nonce: %s\n%!" (Z.to_string r) ;
+
   let r' = Z.(invert r n) and re = Z.(powm r e n) in
   let x  = decrypt_unsafe ~key Z.((re * c) mod n) in
   Z.((r' * x) mod n)
 
 
 let (encrypt_z, decrypt_z) =
-  let aux op f ~key x =
-    if x >= f key then invalid_arg "RSA: key too small" ;
-    if x < Z.one then invalid_arg "RSA: non-positive message" ;
-    op ~key x
+  let check_params n msg =
+    if msg >= n then invalid_arg "RSA: key too small" ;
+    if msg < Z.one then invalid_arg "RSA: non-positive message"
   in
-  (aux encrypt_unsafe         (fun k -> k.n)),
-  (aux decrypt_blinded_unsafe (fun k -> k.n))
+  (fun    ~(key : pub ) msg ->
+    check_params key.n msg ; encrypt_unsafe ~key msg),
+  (fun ?g ~(key : priv) msg ->
+    check_params key.n msg ; decrypt_blinded_unsafe ?g ~key msg)
 
-let encrypt ~key cs = Numeric.Z.(to_cstruct @@ encrypt_z ~key (of_cstruct cs))
-let decrypt ~key cs = Numeric.Z.(to_cstruct @@ decrypt_z ~key (of_cstruct cs))
+let encrypt    ~key cs = Numeric.Z.(to_cstruct @@ encrypt_z    ~key (of_cstruct cs))
+let decrypt ?g ~key cs = Numeric.Z.(to_cstruct @@ decrypt_z ?g ~key (of_cstruct cs))
 
 
 (* XXX
  * This is fishy. Most significant bit is always set to avoid reducing the
  * modulus, but this drops 1 bit of randomness. Investigate.
  *)
-let rec random_prime ?mix bits =
+let rec random_prime ?g ?mix bits =
   let lead = match mix with
     | Some x -> x
     | None   -> Z.(pow (of_int 2)) (bits - 1) in
-  let z = Z.(Rng.Z.gen_bits bits lor lead) in
+  let z = Z.(Rng.Z.gen_bits ?g bits lor lead) in
 (*   Z.nextprime z *)
   match Z.probab_prime z 25 with
-  | 0 -> random_prime ~mix:lead bits
+  | 0 -> random_prime ?g ~mix:lead bits
   | _ -> z
 
 (* XXX
  * All kinds bad. Default public exponent should probably be smaller than
  * 2^16+1. Two bits of key are rigged.
  *)
-let generate ?(e = Z.of_int 0x10001) bits =
+let generate ?g ?(e = Z.of_int 0x10001) bits =
 
   Printf.printf "DON'T use this to generate actual keys.\n%!";
 
   let (p, q) =
     let rec attempt bits =
       (* xxx *)
-      let (p, q) = (random_prime bits, random_prime bits) in
+      let (p, q) = (random_prime ?g bits, random_prime ?g bits) in
       match p = q with
       | false when Z.(gcd e (pred p) = one) &&
                    Z.(gcd e (pred q) = one) -> (p, q)
