4d7293ae420613d86d224ef4dbe893002c2c3b95
pqwy <unitedbiscuits@gmail.com>
2014-03-21 01:41:38 +0100

    some standard DH groups

--- src/dh.mli
+++ src/dh.mli
@@ -6,3 +6,8 @@ val gen_params : ?g:Rng.g -> int -> params
 val gen_secret : ?g:Rng.g -> params -> secret * Cstruct.t
 val shared     : params -> secret -> Cstruct.t -> Cstruct.t
 
+module Params : sig
+  val rfc_5114_1 : params
+  val rfc_5114_2 : params
+  val rfc_5114_3 : params
+end
