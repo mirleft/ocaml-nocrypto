46ffd7f49163d19edab3288a8cf60814258ae3e9
Hannes Mehnert <hannes@mehnert.org>
2014-03-27 21:17:01 +0100

    here's b64decode

--- src/base64.ml
+++ src/base64.ml
@@ -9,8 +9,8 @@ let (emap, dmap) =
     let ht = Hashtbl.create 64 in
     for i = 0 to String.length sym - 1 do f ht i sym.[i] done ;
     ht in
-  (lazy (make_ht Hashtbl.add)),
-  (lazy (make_ht (fun ht i c -> Hashtbl.add ht c i)))
+  (make_ht Hashtbl.add),
+  (make_ht (fun ht i c -> Hashtbl.add ht c i))
 
 
 let padding_size cs =
@@ -20,9 +20,39 @@ let padding_size cs =
   | 1 -> is_pad 0
   | n -> is_pad (n - 1) + is_pad (n - 2)
 
-(* let decode cs =
+let decode cs =
   let n  = len cs in
-  let n' = n / 4 - padding_size cs in
-  let rec loop = function
-    |  *)
-
+  let n' = 3 * n / 4 - padding_size cs in
+  let r = create n' in
+  let rec go leftover bits rbyte wbyte =
+    if rbyte >= n then
+      ()
+    else
+      if bits >= 8 then
+        ( let nbits = bits - 8 in
+          let tval = (leftover asr nbits) land 0xFF in
+          let lo = leftover land (pred (1 lsl nbits)) in
+          Printf.printf "bits %d nbits %d tval %d tvalc %c leftover %d\n"
+                        bits nbits tval (char_of_int tval) lo;
+          set_uint8 r wbyte tval;
+          go lo
+             nbits
+             rbyte
+             (succ wbyte) )
+      else
+        let ch = char_of_int (get_uint8 cs rbyte) in
+        Printf.printf "got character %c\n" ch;
+        if ch = '=' then
+          ()
+        else
+          let dec = Hashtbl.find dmap ch in
+          Printf.printf "decoded character %d\n" dec;
+          (* we better ensure dec is sane (and byte was in dmap) *)
+          (* dec is 6 bit, thus shift leftover by 6 *)
+          go ((leftover lsl 6) + dec)
+             (bits + 6)
+             (succ rbyte)
+             wbyte
+  in
+  go 0 0 0 0;
+  r
