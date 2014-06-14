60e1285d80c3d662f86e45cc411d556ba111207f
pqwy <david@numm.org>
2014-06-14 01:33:59 +0100

    more entropy in tests

--- tests/testrunner.ml
+++ tests/testrunner.ml
@@ -1,6 +1,13 @@
 
 open OUnit2
 
+(* Gather quantum uncertainty. *)
 let () =
-  Nocrypto.Rng.reseed @@ Cstruct.of_string "\001\002\003\004" ;
+  let t  = Unix.gettimeofday () in
+  let cs = Cstruct.create 8 in
+  Cstruct.BE.set_uint64 cs 0 Int64.(of_float (t *. 1000.)) ;
+  Nocrypto.Rng.reseed cs
+
+let () =
+(*   Nocrypto.Rng.reseed @@ Cstruct.of_string "\001\002\003\004" ; *)
   run_test_tt_main Testlib.suite
