279a4197bde67fd0a36226935544e325c01ab1c4
Hannes Mehnert <hannes@mehnert.org>
2014-07-29 21:00:58 +0100

    initial working version

---
+++ src/ccm.ml
@@ -0,0 +1,200 @@
+
+open Cstruct
+
+  let append cs1 cs2 =
+    let l1 = len cs1 and l2 = len cs2 in
+    let cs = create (l1 + l2) in
+    blit cs1 0 cs 0 l1 ;
+    blit cs2 0 cs l1 l2 ;
+    cs
+
+let (<+>) = append
+
+
+let valid_t = [ 4 ; 6 ; 8 ; 10 ; 12 ; 14 ; 16 ] (* octet length of mac *)
+let valid_small_q = [ 2 ; 3 ; 4 ; 5 ; 6 ; 7 ; 8 ] (* octet length of octet length of plain *)
+let valid_n = [ 7 ; 8 ; 9 ; 10 ; 11 ; 12 ; 13 ] (* octet length of nonce *)
+
+let bits n =
+  let rec go acc = function
+    | 0 -> acc
+    | n -> go (acc + 1) (n lsr 1)
+  in
+  go 0 n
+
+let format nonce adata plain t (* mac len *) =
+  let q = len plain in
+  (* n + q = 15 *)
+  (* a < 2 ^ 64 *)
+  let n = len nonce in
+  let small_q = 15 - n in
+  assert (List.mem small_q valid_small_q) ;
+  assert (List.mem t valid_t) ;
+  assert (List.mem n valid_n) ;
+  assert (small_q > 0 && small_q > bits q) ;
+  (* first byte (flags): *)
+  (* reserved | adata | (t - 2) / 2 | q - 1 *)
+  let flags =
+    let b6 = match adata with
+      | Some _ -> 1
+      | None   -> 0
+    and b543 = (t - 2) / 2
+    and b210 = small_q - 1
+    in
+    let value = (b6 lsl 6) + (b543 lsl 3) + b210
+    and byte = create 1 in
+    set_uint8 byte 0 value ;
+    byte
+  in
+  (* first octet block:
+     0          : flags
+     1..15 - q  : N
+     16 - q..15 : Q *)
+  let qblock =
+    let b = create small_q in
+    let rec ass num = function
+      | 0 -> set_uint8 b 0 num ; b
+      | m -> set_uint8 b m (num land 0xff) ; ass (num lsr 8) (m - 1)
+    in
+    ass q (small_q - 1);
+  in
+  flags <+> nonce <+> qblock
+
+let cs_of_list l =
+  let b = create (List.length l) in
+  let rec go idx = function
+    | []    -> ()
+    | x::xs -> set_uint8 b idx x ; go (idx + 1) xs
+  in
+  go 0 l ;
+  b
+
+let create0 n =
+  let b = create n in
+  let rec go = function
+    | m when n = m -> b
+    | m            -> set_uint8 b m 0 ; go (m + 1)
+  in
+  go 0
+
+let pad16 b =
+  let pad = create0 (16 - len b mod 16) in
+  b <+> pad
+
+let gen_adata a =
+  let lbuf =
+    match len a with
+    | x when x < (1 lsl 16 - 1 lsl 8) ->
+       let buf = create 2 in
+       BE.set_uint16 buf 0 x ;
+       buf
+    | x when x < (1 lsl 32)           ->
+       let buf = create 4 in
+       BE.set_uint32 buf 0 (Int32.of_int x) ;
+       cs_of_list [0xff ; 0xfe] <+> buf
+    | x                               ->
+       let buf = create 8 in
+       BE.set_uint64 buf 0 (Int64.of_int x) ;
+       cs_of_list [0xff ; 0xff] <+> buf
+  in
+  pad16 (lbuf <+> a)
+
+let gen_ctr nonce i =
+  let n = len nonce in
+  let small_q = 15 - n in
+  let flags =
+    let b = create 1 in
+    set_uint8 b 0 (small_q - 1) ;
+    b
+  in
+  let count =
+    let b = create small_q in
+    let rec ass num = function
+      | 0 -> set_uint8 b 0 num ; b
+      | m -> set_uint8 b m (num land 0xff) ; ass (num lsr 8) (m - 1)
+    in
+    ass i (small_q - 1);
+  in
+  flags <+> nonce <+> count
+
+
+let a = cs_of_list [ 0x00 ; 0x01 ; 0x02 ; 0x03 ; 0x04 ; 0x05 ; 0x06 ; 0x07 ]
+let n = cs_of_list [ 0x10 ; 0x11 ; 0x12 ; 0x13 ; 0x14 ; 0x15 ; 0x16 ]
+let p = cs_of_list [ 0x20 ; 0x21 ; 0x22 ; 0x23 ]
+let k = Block_cipher.AES.Raw.e_of_secret (cs_of_list [ 0x40 ; 0x41 ; 0x42 ; 0x43 ; 0x44 ; 0x45 ; 0x46 ; 0x47 ; 0x48 ; 0x49 ; 0x4A ; 0x4B ; 0x4C ; 0x4D ; 0x4E ; 0x4F ])
+
+let ccm key nonce ?adata data tlen =
+  let ada = match adata with
+    | Some x -> gen_adata x
+    | None   -> create0 0
+  in
+  let bs = (format nonce adata data tlen) <+> ada <+> pad16 data in
+  let rec loop last block =
+    match len block with
+    | 0 -> last
+    | _ ->
+       Common.Cs.xor_into last block 16 ;
+       Block_cipher.AES.Raw.encrypt_block ~key block block ;
+       loop (sub block 0 16)
+            (shift block 16)
+  in
+  let last = loop (create0 16) bs in
+  let t = sub last 0 tlen in
+  Cstruct.hexdump t ;
+  let ctrblocks = Common.cdiv (len data) 16 + 1 in
+  let rec ctrloop idx = function
+    | 0 -> []
+    | n ->
+       let ctr = gen_ctr nonce idx in
+       Block_cipher.AES.Raw.encrypt_block ~key ctr ctr ;
+       ctr :: ctrloop (idx + 1) (n - 1)
+  in
+  let blocks = ctrloop 0 ctrblocks in
+  Printf.printf "ctrblocks" ;
+  List.iter hexdump blocks ;
+  let block0 = sub (List.hd blocks) 0 (len p) in
+  let block1 = sub (List.hd (List.tl blocks)) 0 (len t) in
+  Common.Cs.xor_into block1 p (len p) ;
+  Common.Cs.xor_into block0 t (len t) ;
+  let c = p <+> t in
+  Printf.printf "c" ;
+  hexdump c
+
+let decrypt key nonce ?adata cipher tlen =
+  let pclen = len cipher - tlen in
+  assert (pclen > 0);
+  let ctrblocks = Common.cdiv pclen 16 + 1 in
+  let rec ctrloop idx = function
+    | 0 -> []
+    | n ->
+       let ctr = gen_ctr nonce idx in
+       Block_cipher.AES.Raw.encrypt_block ~key ctr ctr ;
+       ctr :: ctrloop (idx + 1) (n - 1)
+  in
+  let blocks = ctrloop 0 ctrblocks in
+  let block1 = List.hd (List.tl blocks) in
+  Common.Cs.xor_into block1 cipher pclen ;
+  let p = sub cipher 0 pclen in
+  let block0 = List.hd blocks in
+  Common.Cs.xor_into block0 (sub cipher pclen tlen) tlen ;
+  let t = sub cipher pclen tlen in
+  Printf.printf "t" ; hexdump t ;
+
+  let ada = match adata with
+    | Some x -> gen_adata x
+    | None   -> create0 0
+  in
+  let bs = (format nonce adata p tlen) <+> ada <+> pad16 p in
+  let rec loop last block =
+    match len block with
+    | 0 -> last
+    | _ ->
+       Common.Cs.xor_into last block 16 ;
+       Block_cipher.AES.Raw.encrypt_block ~key block block ;
+       loop (sub block 0 16)
+            (shift block 16)
+  in
+  let last = loop (create0 16) bs in
+  let t' = sub last 0 tlen in
+  (* assert t' = t *)
+  Printf.printf "t'" ; hexdump t'
