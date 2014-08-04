b14545d9822f76141f382f8d44755a9dac713c64
pqwy <david@numm.org>
2014-08-04 16:12:35 +0100

    properly hook up header-inspection

--- src_gen/nocrypto_generated_sizes.ml
+++
@@ -1,9 +0,0 @@
-
-(*
- * Kludgy way to extract info from C headers.
- *
- * Machine-dependent; should not be commited. Should not exist even.
- *)
-
-let sizeof_MD5_CTX = 152
-let sizeof_SHA_CTX = 208
