f00bcc2719fa3b98affb0f95f3b9ee7a00b7651b
pqwy <david@numm.org>
2014-08-04 19:10:18 +0100

    remove mention of a fixed bug

--- src/native.ml
+++ src/native.ml
@@ -10,9 +10,8 @@ module Conv = struct
   let bs_ptr bs = bigarray_start array1 bs
 
   let cs_ptr cs =
-(*     bigarray_start array1 Cstruct.(to_bigarray cs) *)
-    (* XXX https://github.com/ocamllabs/ocaml-ctypes/issues/165 *)
     bigarray_start array1 cs.Cstruct.buffer +@ cs.Cstruct.off
+(*     bigarray_start array1 Cstruct.(to_bigarray cs) *)
 
   let cs_len_size_t cs = Unsigned.Size_t.of_int cs.Cstruct.len
 
