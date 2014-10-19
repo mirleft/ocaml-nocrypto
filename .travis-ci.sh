ef080c794d42d4f399a3dd1cae5561cedd6d17b7
Hannes Mehnert <hannes@mehnert.org>
2014-10-19 13:03:24 +0200

    require a new cstruct version (ocaml 4.1 and opam 1.1 seem to select an old version on travis)

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # OPAM packages needed to build tests.
-OPAM_PACKAGES="cstruct ctypes zarith oUnit"
+OPAM_PACKAGES="sexplib cstruct ctypes zarith oUnit"
 
 case "$OCAML_VERSION" in
     4.01.0) ppa=avsm/ocaml41+opam11 ;;
