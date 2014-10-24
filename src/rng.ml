b6a513781be88c6f9859b5c6a7dbad5b06235268
pqwy <david@numm.org>
2014-10-24 23:57:15 +0200

    better separation of modes

--- src/rng.ml
+++ src/rng.ml
@@ -6,43 +6,51 @@ module Numeric_of (Rng : Random.Rng) = struct
   type g = Rng.g
 
   let relax = ref true
+  let strict s = relax := not s
 
   module N_gen (N : Numeric.T) = struct
 
     type t = N.t
     type g = Rng.g
 
-    let gen ?g n =
-      if n < N.one then invalid_arg "rng: non-positive bound" ;
-
-      let bits   = N.bits (N.pred n)
-      and items  = if !relax then 2 else 1 in
+    (* Non-strict: less wasteful in driving the RNG. *)
+    let gen_ns ?g n =
+      let bits   = N.(bits (pred n)) in
       let octets = cdiv bits 8 in
-      let batch  = Rng.(block_size * cdiv (octets * items) block_size) in
+      let batch  = Rng.(block_size * cdiv (octets * 2) block_size) in
 
       let rec attempt cs =
-        try
+        if cs.Cstruct.len >= octets then
           let x = N.of_cstruct_be ~bits cs in
           if x < n then x else attempt (Cstruct.shift cs octets)
-        with | Invalid_argument _ -> attempt Rng.(generate ?g batch) in
+        else attempt Rng.(generate ?g batch) in
       attempt Rng.(generate ?g batch)
 
-    let gen_r ?g a b = N.(a + gen ?g (b - a))
+    let gen_r_ns ?g a b = N.(a + gen_ns ?g (b - a))
 
-    let rec gen_r_strict ?g a b =
-      let x = gen ?g b in
-      if x < a then gen_r_strict ?g a b else a
+    (* Strict: close to how extraction is described in various standards. *)
+    let gen_s ?g n =
+      let bits   = N.(bits (pred n)) in
+      let octets = cdiv bits 8 in
+      let rec attempt () =
+        let x = N.of_cstruct_be ~bits Rng.(generate ?g octets) in
+        if x < n then x else attempt () in
+      attempt ()
 
-    let gen_r ?g a b = (if !relax then gen_r else gen_r_strict) ?g a b
+    let rec gen_r_s ?g a b =
+      let x = gen_s ?g b in if x < a then gen_r_s ?g a b else x
+
+    (* Common: reacts to a toggle. The lesser evil... *)
+    let gen ?g n =
+      if n < N.one then invalid_arg "Rng.gen: non-positive bound";
+      (if !relax then gen_ns else gen_s) ?g n
+
+    let gen_r ?g a b =
+      (if !relax then gen_r_ns else gen_r_s) ?g a b
 
     let gen_bits ?g bits =
-      let octets = cdiv bits 8 in
-      let cs     = Rng.(generate ?g octets) in
-      N.of_cstruct_be ~bits cs
-
+      N.of_cstruct_be ~bits Rng.(generate ?g (cdiv bits 8))
   end
-
-  let strict s = relax := not s
 
   module Int   = N_gen (Numeric.Int  )
   module Int32 = N_gen (Numeric.Int32)
