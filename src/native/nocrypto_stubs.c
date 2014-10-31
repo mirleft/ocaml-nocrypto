88283186a026f8c08c61974b75df8fcff571994a
pqwy <david@numm.org>
2014-10-31 02:10:21 +0100

    move our shims; size-getters

--- src/native/nocrypto_stubs.c
+++ src/native/nocrypto_stubs.c
@@ -1,9 +1,14 @@
 #include "nocrypto_stubs.h"
 
 size_t nocrypto_stub_sizeof_md5_ctx () {
-  return sizeof (MD5_CTX);
+  return MD5_CTX_SIZE;
 }
-
-size_t nocrypto_stub_sizeof_sha_ctx () {
-  return sizeof (SHA_CTX);
+size_t nocrypto_stub_sizeof_sha1_ctx () {
+  return SHA1_CTX_SIZE;
+}
+size_t nocrypto_stub_sizeof_sha256_ctx () {
+  return SHA256_CTX_SIZE;
+}
+size_t nocrypto_stub_sizeof_sha512_ctx () {
+  return SHA512_CTX_SIZE;
 }
--- src/native/nocrypto_stubs.h
+++ src/native/nocrypto_stubs.h
@@ -1,7 +1,14 @@
-#include "sha2.h"
+#include "stddef.h" // size_t
+#include "string.h" // memset
+
 #include "md5.h"
+#include "sha1.h"
+#include "sha256.h"
+#include "sha512.h"
 #include "rijndael.h"
 #include "d3des.h"
 
 size_t nocrypto_stub_sizeof_md5_ctx ();
-size_t nocrypto_stub_sizeof_sha_ctx ();
+size_t nocrypto_stub_sizeof_sha1_ctx ();
+size_t nocrypto_stub_sizeof_sha256_ctx ();
+size_t nocrypto_stub_sizeof_sha512_ctx ();
