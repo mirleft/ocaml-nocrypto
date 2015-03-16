67d6eac8defb867075fe6476fbc1ee04ec75eda0
pqwy <david@numm.org>
2015-03-16 00:02:50 +0000

    stick more native files into places

--- src/native/nocrypto_stubs.h
+++ src/native/nocrypto_stubs.h
@@ -1,10 +1,10 @@
 #include "stddef.h" // size_t
 #include "string.h" // memset
 
-#include "md5.h"
-#include "sha1.h"
-#include "sha256.h"
-#include "sha512.h"
+#include "hash/md5.h"
+#include "hash/sha1.h"
+#include "hash/sha256.h"
+#include "hash/sha512.h"
 
 #define CTX_SIZE_GETTER_DECL(HASH) \
   size_t nocrypto_sizeof_ ## HASH ## _ctx ();
