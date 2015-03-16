d4118101f69d92d6d34a2b617ee4306519c71847
pqwy <david@numm.org>
2015-03-16 00:04:20 +0000

    hash bindings

--- src/native.ml
+++ src/native.ml
@@ -22,6 +22,50 @@ module DES = struct
   external k_s     : unit -> int                                    = "caml_nc_des_key_size" "noalloc"
 end
 
+type ctx = buffer
+
+module MD5 = struct
+  external init     : ctx -> unit                         = "caml_nc_md5_init"     "noalloc"
+  external update   : ctx -> buffer -> int -> int -> unit = "caml_nc_md5_update"   "noalloc"
+  external finalize : ctx -> buffer -> int -> unit        = "caml_nc_md5_finalize" "noalloc"
+  external ctx_size : unit -> int                         = "caml_nc_md5_ctx_size" "noalloc"
+end
+
+module SHA1 = struct
+  external init     : ctx -> unit                         = "caml_nc_sha1_init"     "noalloc"
+  external update   : ctx -> buffer -> int -> int -> unit = "caml_nc_sha1_update"   "noalloc"
+  external finalize : ctx -> buffer -> int -> unit        = "caml_nc_sha1_finalize" "noalloc"
+  external ctx_size : unit -> int                         = "caml_nc_sha1_ctx_size" "noalloc"
+end
+
+module SHA224 = struct
+  external init     : ctx -> unit                         = "caml_nc_sha224_init"     "noalloc"
+  external update   : ctx -> buffer -> int -> int -> unit = "caml_nc_sha224_update"   "noalloc"
+  external finalize : ctx -> buffer -> int -> unit        = "caml_nc_sha224_finalize" "noalloc"
+  external ctx_size : unit -> int                         = "caml_nc_sha224_ctx_size" "noalloc"
+end
+
+module SHA256 = struct
+  external init     : ctx -> unit                         = "caml_nc_sha256_init"     "noalloc"
+  external update   : ctx -> buffer -> int -> int -> unit = "caml_nc_sha256_update"   "noalloc"
+  external finalize : ctx -> buffer -> int -> unit        = "caml_nc_sha256_finalize" "noalloc"
+  external ctx_size : unit -> int                         = "caml_nc_sha256_ctx_size" "noalloc"
+end
+
+module SHA384 = struct
+  external init     : ctx -> unit                         = "caml_nc_sha384_init"     "noalloc"
+  external update   : ctx -> buffer -> int -> int -> unit = "caml_nc_sha384_update"   "noalloc"
+  external finalize : ctx -> buffer -> int -> unit        = "caml_nc_sha384_finalize" "noalloc"
+  external ctx_size : unit -> int                         = "caml_nc_sha384_ctx_size" "noalloc"
+end
+
+module SHA512 = struct
+  external init     : ctx -> unit                         = "caml_nc_sha512_init"     "noalloc"
+  external update   : ctx -> buffer -> int -> int -> unit = "caml_nc_sha512_update"   "noalloc"
+  external finalize : ctx -> buffer -> int -> unit        = "caml_nc_sha512_finalize" "noalloc"
+  external ctx_size : unit -> int                         = "caml_nc_sha512_ctx_size" "noalloc"
+end
+
 (* XXX TODO
  * Unsolved: bounds-checked XORs are slowing things down considerably... *)
 external xor_into : buffer -> int -> buffer -> int -> int -> unit = "caml_nc_xor_into" "caml_nc_xor_into" "noalloc"
