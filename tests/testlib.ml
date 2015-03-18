9ebb347b96f84dd3c5c799edcb81df2898dd042b
pqwy <david@numm.org>
2015-03-18 19:28:12 +0000

    SP 800-38A ECB tests

--- tests/testlib.ml
+++ tests/testlib.ml
@@ -422,6 +422,38 @@ let nist_sp_800_38a = Cs.of_hex
    30 c8 1c 46 a3 5c e4 11 e5 fb c1 19 1a 0a 52 ef
    f6 9f 24 45 df 4f 9b 17 ad 2b 41 7b e6 6c 37 10"
 
+let aes_ecb_cases =
+  let open Cipher_block in
+
+  let case ~key ~out = Cs.(AES.ECB.of_secret (of_hex key), of_hex out)
+
+  and check (key, out) _ =
+    let enc = AES.ECB.encrypt ~key nist_sp_800_38a in
+    let dec = AES.ECB.decrypt ~key enc in
+    assert_cs_equal ~msg:"cyphertext" out enc ;
+    assert_cs_equal ~msg:"plaintext" nist_sp_800_38a dec in
+
+  cases_of check [
+    case ~key: "2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c"
+         ~out: "3a d7 7b b4 0d 7a 36 60 a8 9e ca f3 24 66 ef 97
+                f5 d3 d5 85 03 b9 69 9d e7 85 89 5a 96 fd ba af
+                43 b1 cd 7f 59 8e ce 23 88 1b 00 e3 ed 03 06 88
+                7b 0c 78 5e 27 e8 ad 3f 82 23 20 71 04 72 5d d4"
+  ; case ~key: "8e 73 b0 f7 da 0e 64 52 c8 10 f3 2b 80 90 79 e5
+                62 f8 ea d2 52 2c 6b 7b"
+         ~out: "bd 33 4f 1d 6e 45 f2 5f f7 12 a2 14 57 1f a5 cc
+                97 41 04 84 6d 0a d3 ad 77 34 ec b3 ec ee 4e ef
+                ef 7a fd 22 70 e2 e6 0a dc e0 ba 2f ac e6 44 4e
+                9a 4b 41 ba 73 8d 6c 72 fb 16 69 16 03 c1 8e 0e"
+  ; case ~key: "60 3d eb 10 15 ca 71 be 2b 73 ae f0 85 7d 77 81
+                1f 35 2c 07 3b 61 08 d7 2d 98 10 a3 09 14 df f4"
+         ~out: "f3 ee d1 bd b5 d2 a0 3c 06 4b 5a 7e 3d b1 81 f8
+                59 1c cb 10 d4 10 ed 26 dc 5b a7 4a 31 36 28 70
+                b6 ed 21 b9 9c a6 f4 f9 f1 53 e7 b1 be af ed 1d
+                23 30 4b 7a 39 f9 f3 ff 06 7d 8d 8f 9e 24 ec c7"
+  ]
+
+
 let aes_ctr_cases =
   let open Cipher_block in
 
@@ -432,8 +464,7 @@ let aes_ctr_cases =
     let enc = AES.CTR2.encrypt ~key ~ctr nist_sp_800_38a in
     let dec = AES.CTR2.decrypt ~key ~ctr enc in
     assert_cs_equal ~msg:"cyphertext" out enc ;
-    assert_cs_equal ~msg:"plaintext" nist_sp_800_38a dec
-    in
+    assert_cs_equal ~msg:"plaintext" nist_sp_800_38a dec in
 
   cases_of check [
     case ~key: "2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c"
@@ -707,12 +738,13 @@ let suite =
 
     "3DES-CBC" >::: [ cbc_selftest (module Cipher_block.DES.CBC) 100 ] ;
 
-    "AES-ECB" >::: [ ecb_selftest (module Cipher_block.AES.ECB) 100 ] ;
+    "AES-ECB" >::: [ ecb_selftest (module Cipher_block.AES.ECB) 100
+                   ; "SP 300-38A" >::: aes_ecb_cases ] ;
 
     "AES-CBC" >::: [ cbc_selftest (module Cipher_block.AES.CBC) 100 ] ;
 
     "AES-CTR" >::: [ ctr_selftest (module Cipher_block.AES.CTR2) 100
-                   ; "NIST SP 300-38A" >::: aes_ctr_cases ] ;
+                   ; "SP 300-38A" >::: aes_ctr_cases ] ;
 
     "AES-GCM" >::: gcm_cases ;
 
