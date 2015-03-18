9595586dd11f96ef47fb44381db56ac2a40163a0
pqwy <david@numm.org>
2015-03-18 19:36:37 +0000

    SP 800-38A CBC tests

--- tests/testlib.ml
+++ tests/testlib.ml
@@ -439,12 +439,14 @@ let aes_ecb_cases =
                 f5 d3 d5 85 03 b9 69 9d e7 85 89 5a 96 fd ba af
                 43 b1 cd 7f 59 8e ce 23 88 1b 00 e3 ed 03 06 88
                 7b 0c 78 5e 27 e8 ad 3f 82 23 20 71 04 72 5d d4"
+
   ; case ~key: "8e 73 b0 f7 da 0e 64 52 c8 10 f3 2b 80 90 79 e5
                 62 f8 ea d2 52 2c 6b 7b"
          ~out: "bd 33 4f 1d 6e 45 f2 5f f7 12 a2 14 57 1f a5 cc
                 97 41 04 84 6d 0a d3 ad 77 34 ec b3 ec ee 4e ef
                 ef 7a fd 22 70 e2 e6 0a dc e0 ba 2f ac e6 44 4e
                 9a 4b 41 ba 73 8d 6c 72 fb 16 69 16 03 c1 8e 0e"
+
   ; case ~key: "60 3d eb 10 15 ca 71 be 2b 73 ae f0 85 7d 77 81
                 1f 35 2c 07 3b 61 08 d7 2d 98 10 a3 09 14 df f4"
          ~out: "f3 ee d1 bd b5 d2 a0 3c 06 4b 5a 7e 3d b1 81 f8
@@ -453,6 +455,42 @@ let aes_ecb_cases =
                 23 30 4b 7a 39 f9 f3 ff 06 7d 8d 8f 9e 24 ec c7"
   ]
 
+let aes_cbc_cases =
+  let open Cipher_block in
+
+  let case ~key ~iv ~out =
+    Cs.(AES.CBC.of_secret (of_hex key), of_hex iv, of_hex out)
+
+  and check (key, iv, out) _ =
+    let { AES.CBC.message = enc ; _ } = AES.CBC.encrypt ~key ~iv nist_sp_800_38a in
+    let { AES.CBC.message = dec ; _ } = AES.CBC.decrypt ~key ~iv enc in
+    assert_cs_equal ~msg:"cyphertext" out enc ;
+    assert_cs_equal ~msg:"plaintext" nist_sp_800_38a dec in
+
+  cases_of check [
+    case ~key: "2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c"
+         ~iv:  "00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f"
+         ~out: "76 49 ab ac 81 19 b2 46 ce e9 8e 9b 12 e9 19 7d
+                50 86 cb 9b 50 72 19 ee 95 db 11 3a 91 76 78 b2
+                73 be d6 b8 e3 c1 74 3b 71 16 e6 9e 22 22 95 16
+                3f f1 ca a1 68 1f ac 09 12 0e ca 30 75 86 e1 a7"
+
+  ; case ~key: "8e 73 b0 f7 da 0e 64 52 c8 10 f3 2b 80 90 79 e5
+                62 f8 ea d2 52 2c 6b 7b"
+         ~iv:  "00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f"
+         ~out: "4f 02 1d b2 43 bc 63 3d 71 78 18 3a 9f a0 71 e8
+                b4 d9 ad a9 ad 7d ed f4 e5 e7 38 76 3f 69 14 5a
+                57 1b 24 20 12 fb 7a e0 7f a9 ba ac 3d f1 02 e0
+                08 b0 e2 79 88 59 88 81 d9 20 a9 e6 4f 56 15 cd"
+
+  ; case ~key: "60 3d eb 10 15 ca 71 be 2b 73 ae f0 85 7d 77 81
+                1f 35 2c 07 3b 61 08 d7 2d 98 10 a3 09 14 df f4"
+         ~iv:  "00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f"
+         ~out: "f5 8c 4c 04 d6 e5 f1 ba 77 9e ab fb 5f 7b fb d6
+                9c fc 4e 96 7e db 80 8d 67 9f 77 7b c6 70 2c 7d
+                39 f2 33 69 a9 d9 ba cf a5 30 e2 63 04 23 14 61
+                b2 eb 05 e2 c3 9b e9 fc da 6c 19 07 8c 6a 9d 1b"
+  ]
 
 let aes_ctr_cases =
   let open Cipher_block in
@@ -741,7 +779,8 @@ let suite =
     "AES-ECB" >::: [ ecb_selftest (module Cipher_block.AES.ECB) 100
                    ; "SP 300-38A" >::: aes_ecb_cases ] ;
 
-    "AES-CBC" >::: [ cbc_selftest (module Cipher_block.AES.CBC) 100 ] ;
+    "AES-CBC" >::: [ cbc_selftest (module Cipher_block.AES.CBC) 100
+                   ; "SP 300-38A" >::: aes_cbc_cases ] ;
 
     "AES-CTR" >::: [ ctr_selftest (module Cipher_block.AES.CTR2) 100
                    ; "SP 300-38A" >::: aes_ctr_cases ] ;
