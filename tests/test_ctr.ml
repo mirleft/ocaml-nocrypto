019ccea780b2bcd0819dd513d8efc19f4a56fef3
NASR ALLAH Mounir <nasrallah.mounir@gmail.com>
2015-04-20 14:49:25 +0100

    Printing results improved.

--- tests/test_ctr.ml
+++ tests/test_ctr.ml
@@ -9,17 +9,11 @@ and msg = Cstruct.of_string "fire the missile because now there are some nasties
 
 
 let _ =
-  let _ = Printf.printf "Initial CTR:\t%s \n" (Cstruct.to_string ctr) in
-  let _ = Printf.printf "----------------------------------------------------- \n" in
-  let _ = Printf.printf "Original:\t%s \n" (Cstruct.to_string msg) in
-  let _ = Printf.printf "----------------------------------------------------- \n" in
+  let _ = Printf.printf "\nInitial CTR:\t\t\t%s \n" (Cstruct.to_string ctr) in
+  let _ = Printf.printf "Original:\t\t\t%s \n" (Cstruct.to_string msg) in
   let enc_result = MyCTR.encrypt ~key ~ctr:ctr msg in
-  let _ = Printf.printf "Encrypted:\t%s \n" (Cstruct.to_string enc_result) in
-  let _ = Printf.printf "----------------------------------------------------- \n" in
-  let _ = Printf.printf "CTR after encryption:\t%s \n" (Cstruct.to_string ctr) in
-  let _ = Printf.printf "----------------------------------------------------- \n" in
+  let _ = Printf.printf "\nEncrypted:\t\t\t%s \n" (Cstruct.to_string enc_result) in
+  let _ = Printf.printf "CTR after encryption:\t\t%s \n" (Cstruct.to_string ctr) in
   let dec_result = MyCTR.decrypt ~key ~ctr:ctr enc_result in
-  let _ = Printf.printf "Decrypted:\t%s \n" (Cstruct.to_string dec_result) in
-  let _ = Printf.printf "----------------------------------------------------- \n" in
-  let _ = Printf.printf "CTR after decryption:\t%s \n" (Cstruct.to_string ctr) in
-  Printf.printf "----------------------------------------------------- \n"
+  let _ = Printf.printf "\nDecrypted:\t\t\t%s \n" (Cstruct.to_string dec_result) in
+   Printf.printf "CTR after decryption:\t\t%s \n\n" (Cstruct.to_string ctr)
