993577316cd5d851eddd836148c4b08293c24c79
pqwy <david@numm.org>
2015-04-21 13:54:22 +0100

    add lwt/unix entropy feeder

---
+++ lwt/nocrypto_entropy_lwt.mli
@@ -0,0 +1,13 @@
+type t
+
+val attach : period:int -> device:string -> Nocrypto.Fortuna.g -> t Lwt.t
+(** Arranges for bytes from [device] to be used to reseed [g] at most every
+ * [period] seconds. *)
+
+val stop : t -> unit Lwt.t
+(** Stops the reseeding process associated with [t]. Idempotent. *)
+
+val initialize : unit -> unit Lwt.t
+(** [attach]es a reseeding process to the current default generator in
+ * [Nocrypto.Rng], stopping the previous process. Idempotent if the global
+ * generator has not been changed. *)
