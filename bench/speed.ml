6d8f1af8f66abaa03cbfb83ff54032afb95ad400
pqwy <david@numm.org>
2015-06-09 01:01:43 +0100

    streamline benchmarks

--- bench/speed.ml
+++ bench/speed.ml
@@ -7,9 +7,9 @@ open Hash
 
 module Time = struct
 
-  let time f =
+  let time ~n f a =
     let t1 = Sys.time () in
-    ignore (f ());
+    for i = 1 to n do ignore (f a) done ;
     let t2 = Sys.time () in
     (t2 -. t1)
 
@@ -31,29 +31,26 @@ let burn_period = 2.0
 
 let sizes = [16; 64; 256; 1024; 8192]
 
-
 let burn f n =
-  let cs    = Rng.generate n in
-  let run x = Time.time @@ fun () ->
-    for i = 1 to x do f cs done in
-  let (t1, n1) =
-    let rec loop n =
-      let t = run n in
-      if t > 0.01 then (t, n) else loop (n * 10) in
+  let cs = Rng.generate n in
+  let (t1, i1) =
+    let rec loop it =
+      let t = Time.time ~n:it f cs in
+      if t > 0.2 then (t, it) else loop (it * 10) in
     loop 10 in
-  let iters = int_of_float (float n1 *. burn_period /. t1) in
-  let time  = run iters in
-  let total = n * iters in
-  (total, time, float total /. time)
+  let iters = int_of_float (float i1 *. burn_period /. t1) in
+  let time  = Time.time ~n:iters f cs in
+  (iters, time, float (n * iters) /. time)
 
 let mb = 1024. *. 1024.
 
 let throughput title f =
   Printf.printf "\n* [%s]\n%!" title ;
   sizes |> List.iter @@ fun size ->
-    let (total, time, bw) = burn f size in
-    Printf.printf "    % 5d:  %04.f MB/s  (%d KB in %.03f s)/s\n%!"
-      size (bw /. mb) (total / 1024) time
+    Gc.full_major () ;
+    let (iters, time, bw) = burn f size in
+    Printf.printf "    % 5d:  %04.f MB/s  (%d iters in %.03f s)\n%!"
+      size (bw /. mb) iters time
 
 let bm name f = (name, fun () -> f name)
 
@@ -62,22 +59,27 @@ let benchmarks = [
   bm "aes-128-ecb" (fun name ->
     let key = AES.ECB.of_secret (Rng.generate 16) in
     throughput name (fun cs -> AES.ECB.encrypt ~key cs)) ;
+
   bm "aes-128-cbc-e" (fun name ->
     let key = AES.CBC.of_secret (Rng.generate 16)
     and iv  = Rng.generate 16 in
     throughput name (fun cs -> AES.CBC.encrypt ~key ~iv cs)) ;
+
   bm "aes-128-cbc-d" (fun name ->
     let key = AES.CBC.of_secret (Rng.generate 16)
     and iv  = Rng.generate 16 in
     throughput name (fun cs -> AES.CBC.decrypt ~key ~iv cs)) ;
+
   bm "aes-128-ctr" (fun name ->
     let key = AES.CTR.of_secret (Rng.generate 16)
     and ctr = Cs.create_with 16 0x00 in
     throughput name (fun cs -> AES.CTR.encrypt ~key ~ctr cs)) ;
+
   bm "aes-128-gcm" (fun name ->
     let key = AES.GCM.of_secret (Rng.generate 16)
     and iv  = Rng.generate 16 in
     throughput name (fun cs -> AES.GCM.encrypt ~key ~iv cs));
+
   bm "aes-128-ccm" (fun name ->
     let key   = AES.CCM.of_secret ~maclen:16 (Rng.generate 16)
     and nonce = Rng.generate 10 in
