63d74931b9e7cadf0ea6fb84431afbf702710ee1
pqwy <david@numm.org>
2015-06-10 01:23:10 +0100

    packing + visibility

--- src/nocrypto.mli
+++ src/nocrypto.mli
@@ -311,75 +311,6 @@ module Cipher_stream : sig
 end
 
 
-(** Implementation of {{: https://www.schneier.com/fortuna.html} Fortuna} CSPRNG. *)
-module Fortuna : sig
-
-  type g
-  (** Generator state. Changes when operated upon. *)
-
-  exception Unseeded_generator
-  (** Thrown when using an uninitialized {!g}. *)
-
-  val block : int
-  (** Internally, generation always produces a multiple of [block] bytes. *)
-
-  val create : unit -> g
-  (** Create new, unseeded {!g}. *)
-
-  val clone  : g:g -> g
-  (** Clone a generator in its current state. *)
-
-  val seeded : g:g -> bool
-  (** [seeded ~g] is [true] iff operations won't throw {!Unseeded_generator}. *)
-
-  val reseed   : g:g -> Cstruct.t -> unit
-  (** [reseed ~g bytes] updates [g] by mixing in [bytes] which should be
-   unpredictable and ideally environmentally sourced. *)
-
-  val reseedv  : g:g -> Cstruct.t list -> unit
-  (** [reseedv ~g list] is like [reseed] with a concatenation of [list], but faster. *)
-
-  val generate : g:g -> int -> Cstruct.t
-  (** [generate ~g n] extracts [n] bytes of random stream from [g]. *)
-
-  val accumulate : g:g -> Rng.S.accumulator Uncommon.one
-  (** [accumulate ~g] is a is a closure that feeds an accumulator pool in a
-      round-robin fashion. *)
-
-  (** Accumulator pools, collecting entropy and periodically reseeding the
-    attached {!g}.
-
-    Reseeding is performed on the first {!generate} following a non-empty
-    sequence of calls to {!add}.
-
-    Each accumulator instance contains 32 entropy pools, which are taken into
-    account with exponentially decreasing frequency and are meant to be fed
-    round-robin.  *)
-  module Accumulator : sig
-
-    type t
-    (** An accumulator. *)
-    val create : g:g -> t
-    (** Creates a new accumulator feeding into [g]. *)
-    val add : acc:t -> source:int -> pool:int -> Cstruct.t -> unit
-    (** [add ~acc ~source ~pool bytes] adds bytes into [pool]-th entropy pool of
-      the accumulator [acc], marked as coming from [source]. [pool] is taken
-      [mod 32] and [source] is taken [mod 256].
-      This operation is fast and is expected to be frequently called with small
-      amounts of environmentally sourced entropy, such as timings or user input.
-      [source] should indicate a stable source of input but has no meaning beyond
-      that. [pool]s should be rotated roughly round-robin.  *)
-  end
-end
-
-
-(** HMAC_DRBG: A NIST-specified RNG based on HMAC construction over the
-    provided hash. *)
-module Hmac_drgb : sig
-  module Make (H : Hash.T) : Rng.S.Generator
-end
-
-
 (** General interface to random number generation. *)
 module Rng : sig
 
@@ -493,6 +424,75 @@ module Rng : sig
   (** [safe_prime ~g bits] gives a prime pair [(g, p)] such that [p = 2g + 1]
       and [p] has [bits] significant bits. *)
 
+end
+
+
+(** Implementation of {{: https://www.schneier.com/fortuna.html} Fortuna} CSPRNG. *)
+module Fortuna : sig
+
+  type g
+  (** Generator state. Changes when operated upon. *)
+
+  exception Unseeded_generator
+  (** Thrown when using an uninitialized {!g}. *)
+
+  val block : int
+  (** Internally, generation always produces a multiple of [block] bytes. *)
+
+  val create : unit -> g
+  (** Create new, unseeded {!g}. *)
+
+  val clone  : g:g -> g
+  (** Clone a generator in its current state. *)
+
+  val seeded : g:g -> bool
+  (** [seeded ~g] is [true] iff operations won't throw {!Unseeded_generator}. *)
+
+  val reseed   : g:g -> Cstruct.t -> unit
+  (** [reseed ~g bytes] updates [g] by mixing in [bytes] which should be
+   unpredictable and ideally environmentally sourced. *)
+
+  val reseedv  : g:g -> Cstruct.t list -> unit
+  (** [reseedv ~g list] is like [reseed] with a concatenation of [list], but faster. *)
+
+  val generate : g:g -> int -> Cstruct.t
+  (** [generate ~g n] extracts [n] bytes of random stream from [g]. *)
+
+  val accumulate : g:g -> Rng.S.accumulator Uncommon.one
+  (** [accumulate ~g] is a is a closure that feeds an accumulator pool in a
+      round-robin fashion. *)
+
+  (** Accumulator pools, collecting entropy and periodically reseeding the
+    attached {!g}.
+
+    Reseeding is performed on the first {!generate} following a non-empty
+    sequence of calls to {!add}.
+
+    Each accumulator instance contains 32 entropy pools, which are taken into
+    account with exponentially decreasing frequency and are meant to be fed
+    round-robin.  *)
+  module Accumulator : sig
+
+    type t
+    (** An accumulator. *)
+    val create : g:g -> t
+    (** Creates a new accumulator feeding into [g]. *)
+    val add : acc:t -> source:int -> pool:int -> Cstruct.t -> unit
+    (** [add ~acc ~source ~pool bytes] adds bytes into [pool]-th entropy pool of
+      the accumulator [acc], marked as coming from [source]. [pool] is taken
+      [mod 32] and [source] is taken [mod 256].
+      This operation is fast and is expected to be frequently called with small
+      amounts of environmentally sourced entropy, such as timings or user input.
+      [source] should indicate a stable source of input but has no meaning beyond
+      that. [pool]s should be rotated roughly round-robin.  *)
+  end
+end
+
+
+(** HMAC_DRBG: A NIST-specified RNG based on HMAC construction over the
+    provided hash. *)
+module Hmac_drgb : sig
+  module Make (H : Hash.T) : Rng.S.Generator
 end
 
 
