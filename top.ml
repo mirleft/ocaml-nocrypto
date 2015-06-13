f34391b499addf22aed6a0450de0caba8088ea27
pqwy <david@numm.org>
2015-06-13 19:34:02 +0100

    top.ml

--- top.ml
+++ top.ml
@@ -1,12 +1,16 @@
-#require "cstruct, zarith, sexplib" ;;
-#directory "_build/src" ;;
-#load "nocrypto.cma";;
-#require "oUnit";;
-#directory "_build/tests";;
-#load "testlib.cma";;
+#require "cstruct, zarith, sexplib"
+#directory "_build/src"
+#load "nocrypto.cma"
 
-#install_printer Z.pp_print ;;
+#require "oUnit"
+#directory "_build/tests"
+#load "testlib.cma"
+
+#directory "_build/unix"
+#load "nocrypto_entropy_unix.cmo"
+
+#install_printer Z.pp_print
 
 open Nocrypto
 
-let _ = Rng.reseed Cstruct.(of_string "abvgd")
+let _ = Nocrypto_entropy_unix.initialize ()
