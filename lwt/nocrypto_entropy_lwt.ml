887b40fd50e333b9fea8c5c2d091f2b3aa6cc2ff
pqwy <david@numm.org>
2015-06-13 19:04:12 +0100

    use unix entropy in lwt, more dox

--- lwt/nocrypto_entropy_lwt.ml
+++ lwt/nocrypto_entropy_lwt.ml
@@ -3,12 +3,13 @@ open Nocrypto
 
 let chunk  = 32
 and period = 30
-and device = "/dev/urandom"
+and device = Nocrypto_entropy_unix.sys_rng
+
 
 type t = {
-  fd : Lwt_unix.file_descr ;
-  nd : (unit -> unit) Lwt_sequence.node ;
-  g  : Rng.g
+  fd     : Lwt_unix.file_descr ;
+  remove : (unit -> unit) Lwt_sequence.node ;
+  g      : Rng.g
 }
 
 let background ~period f =
@@ -24,28 +25,30 @@ let background ~period f =
       async @@ fun () -> f () >|= fun () -> live := false
     end
 
-let attach ~period ~device g =
-  Lwt_unix.(openfile device [O_RDONLY] 0) >>= fun fd ->
+let attach ~period ?(device = device) g =
+  Lwt_unix.(openfile device [O_RDONLY] 0) >|= fun fd ->
   let buf = Cstruct.create chunk in
   let seed () =
     Lwt_cstruct.(complete (read fd) buf) >|= fun () -> Rng.reseed ~g buf in
-  seed () >>= fun () -> return {
-    g ; fd ; nd =
-      Lwt_sequence.add_r (background ~period seed) Lwt_main.enter_iter_hooks
-  }
+  let remove =
+      Lwt_sequence.add_r (background ~period seed) Lwt_main.enter_iter_hooks in
+  { g ; fd ; remove }
 
 let stop t =
-  Lwt_sequence.remove t.nd ;
-  Lwt_unix.close t.fd
+  Lwt_sequence.remove t.remove ;
+  catch (fun () -> Lwt_unix.close t.fd)
+    Unix.(function Unix_error (EBADF, _, _) -> return_unit | exn -> fail exn)
 
 let active = ref None
+and mx     = Lwt_mutex.create ()
 
-(* Totally not concurrent. *)
 let initialize () =
-  let g = !Rng.generator in
-  let register () =
-    attach ~period ~device g >|= fun t -> active := Some t in
-  match !active with
-  | Some t when t.g == g -> return_unit
-  | Some t               -> stop t >>= register
-  | None                 -> register ()
+  Nocrypto_entropy_unix.initialize () ;
+  Lwt_mutex.with_lock mx @@ fun () ->
+    let g      = !Rng.generator in
+    let reg () =
+      attach ~period g >|= fun t -> active := Some t in
+    match !active with
+    | Some t when t.g == g -> return_unit
+    | Some t               -> stop t >>= reg
+    | None                 -> reg ()
--- lwt/nocrypto_entropy_lwt.mli
+++ lwt/nocrypto_entropy_lwt.mli
@@ -1,13 +1,46 @@
-type t
+(** Rng seeding on {b Lwt/Unix}.
 
-val attach : period:int -> device:string -> Nocrypto.Rng.g -> t Lwt.t
-(** Arranges for bytes from [device] to be used to reseed [g] at most every
- * [period] seconds. *)
+    Calling {!initialize} is enough to bring the RNG into a working state. In
+    addition, a background task is set up to periodically reseed the RNG.
+
+    {!initialize} is idempotent as long as {!Nocrypto.Rng.generator} remains
+    unswapped, and it is harmless to call it several times.
+
+    Note that while {!initialize} creates a thread and the init phase is fully
+    completed only after this thread has completed, the initial seeding is done
+    synchronously with the call. This means that there is no need to synchronize
+    to the returned thread to simply start using the RNG.
+
+    Therefore, all of the following is correct usage:
+
+    [let _ = Nocrypto_entropy_lwt.initialize ()]
+
+    [let () =
+      ignore (Nocrypto_entropy_lwt.initialize ());
+      Lwt_main.run (main ())]
+
+    [let () =
+      Lwt_main.run (Nocrypto_entropy_lwt.initialize () >>= main)]
+*)
+
+
+type t
+(** Represents background reseeding process. *)
+
+val attach : period:int -> ?device:string -> Nocrypto.Rng.g -> t Lwt.t
+(** [attach ~period ~device g] instruments the lwt event loop to mix in bytes
+    from [device] into [g] whenever external events cause the loop to wake up,
+    but no more often than once every [period] seconds.
+
+    [device] defaults to {!Nocrypto_entropy_unix.sys_rng}. *)
 
 val stop : t -> unit Lwt.t
 (** Stops the reseeding process associated with [t]. Idempotent. *)
 
 val initialize : unit -> unit Lwt.t
-(** [attach]es a reseeding process to the current default generator in
- * [Nocrypto.Rng], stopping the previous process. Idempotent if the global
- * generator has not been changed. *)
+(** [initialize ()] immediately calls {!Nocrypto_entropy_unix.initialize}, which
+    primes the global generator {!Nocrypto.Rng.generator}. It then calls
+    {!attach} on the global generator.
+
+    If there is a background seeding process started by a previous call, it is
+    stopped. *)
