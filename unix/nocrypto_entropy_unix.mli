43e6462b9ae64e4922555ea0df1e4798b24386aa
pqwy <david@numm.org>
2015-06-13 19:02:56 +0100

    expose sys_rng + more doc

--- unix/nocrypto_entropy_unix.mli
+++ unix/nocrypto_entropy_unix.mli
@@ -1,25 +1,30 @@
-(** A module providing RNG seeding on {b Unix}.
+(** RNG seeding on {b Unix}.
 
     Calling {!initialize} is enough to bring the RNG into a working state.
-    Periodic reseeding is advisable for long-running services. If use are using
-    {b lwt}, {!Nocrypto_entropy_lwt} can do it for you. *)
 
-val reseed : ?bytes:int -> ?device:string -> ?g:Nocrypto.Rng.g -> unit -> unit
-(** [seed ~bytes ~device ~g ()] mixes in [bytes] bytes from the file [device]
-    into the generator [g].
+    {!initialize} is idempotent as long as {!Nocrypto.Rng.generator} remains
+    unswapped, and it is harmless to call it several times.
 
-    This function works well for periodic reseeding of the RNG.
+    Depending on the generator in use, periodic reseeding is advisable for
+    long-running services. You might want to keep invoking {!reseed} on
+    {!Nocrypto.Rng.generator} (or the generator you are using) with a low
+    frequency. If you rely on {b lwt}, check out {!Nocrypto_entropy_lwt}.
+*)
 
-    [g] defaults to the global generator {!Nocrypto.Rng.generator}.
+val sys_rng : string
+(** Detected system RNG device. *)
 
-    [device] is probed for by default and should be the system RNG device.
+val reseed : ?bytes:int -> ?device:string -> Nocrypto.Rng.g -> unit
+(** [reseed ~bytes ~g] mixes in [bytes] bytes from the system RNG as stored in
+    {!sys_rng} into the generator [g].
 
-    [bytes] defaults to a value suitable for reseeding. *)
+    [bytes] default to a small value reasonable for periodic reseeding.
+
+    [device] defaults to {!sys_rng}. *)
 
 val initialize : unit -> unit
 (** Checks if the current global generator {!Nocrypto.Rng.generator} is already
-    seeded. If not, it is seeded from the system RNG device. The function is
-    idempotent as long as the global generator hasn't been swapped.
+    seeded. If not, it is seeded from the system RNG device.
 
     This is the closest thing to {!Random.self_init} and is a good way to prime
     the RNG. *)
