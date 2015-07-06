8ff2fb2c0f31a9552083200779451bd268d8ca95
pqwy <david@numm.org>
2015-07-06 18:32:58 +0100

    headers black magic for OSX
    A clearly sustainable move.

--- src/native/cpudetect_static.c
+++ src/native/cpudetect_static.c
@@ -5,7 +5,14 @@
 #include <cpuid.h>
 
 #define __x86__
+
+/* `clang` calls this `bit_AESNI`. No-one can accuse them of conformism, at least. */
 #define bit_AES (1 << 25)
+
+/* This looks absent in the headers on OSX, even though they derive from a
+ * `clang` which has them. */
+#define signature_INTEL_ebx 0x756e6547
+#define signature_AMD_ebx   0x68747541
 
 #endif
 
