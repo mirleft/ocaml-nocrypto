1771bf2a380c52bacd8183f8671dcfb2c738424c
Michele Orrù <maker@tumbolandia.net>
2016-03-14 21:53:56 +0000

    Fix trailing whitespaces, seriously.

--- src/native/hash/md5.c
+++ src/native/hash/md5.c
@@ -27,8 +27,8 @@
 #include "bitfn.h"
 #include "md5.h"
 
-// Oracle Solaris Studio does not support `#pragma once`, so this is 
-//  a work-around. Unfortunately `hdrstop` is not supported by gcc, 
+// Oracle Solaris Studio does not support `#pragma once`, so this is
+//  a work-around. Unfortunately `hdrstop` is not supported by gcc,
 //  so we need an if-guard around it:
 #ifdef __sun
 # pragma hdrstop
