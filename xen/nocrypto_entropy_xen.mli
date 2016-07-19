ce84f1772c01f4703dcccbfb600e02f1702fdadc
Hannes Mehnert <hannes@mehnert.org>
2016-07-20 00:03:53 +0200

    entropy renaming

--- xen/nocrypto_entropy_xen.mli
+++ xen/nocrypto_entropy_xen.mli
@@ -17,9 +17,9 @@ val initialize : unit -> unit Lwt.t
 
 (** {1 Interface to Mirage Entropy} *)
 
-val sources : unit -> Entropy_xen.source list option
-(** {!Entropy_xen.source}s set up with the last {{!initialize}initialize}. *)
+val sources : unit -> Entropy.source list option
+(** {!Entropy.source}s set up with the last {{!initialize}initialize}. *)
 
-val attach : Entropy_xen.t -> Nocrypto.Rng.g -> Entropy_xen.token Lwt.t
+val attach : Entropy.t -> Nocrypto.Rng.g -> Entropy.token Lwt.t
 (** [attach e g] starts seeding [g] from the entropy provider [e] and returns
     the token to stop the seeding. *)
