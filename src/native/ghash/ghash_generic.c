7d67013f22230290021ab10937d8e72698fab3e9
pqwy <david@numm.org>
2017-12-17 06:15:00 -0500

    (relatively) fast sw GHASH

---
+++ src/native/ghash/ghash_generic.c
@@ -0,0 +1,117 @@
+/* Copyright (c) 2017 David Kaloper Meršinjak. All rights reserved.
+ * See LICENSE.md.
+ *
+ *
+ * Generic table-driven GHASH function.
+ *
+ * References:
+ * - The Galois/Counter Mode of Operation. David A. McGrew and John Viega.
+ * - NIST SP 800-38D. Recommendation for Block Cipher Modes of Operation:
+ *   Galois/Counter Mode (GCM) and GMAC.
+ *
+ * */
+
+/*  LARGE_TABLES -> 65K per key
+ * !LARGE_TABLES -> 8K per key, ~3x slower. */
+#define __NC_GHASH_LARGE_TABLES
+
+#include "../nocrypto.h"
+#if !defined (__nc_PCLMUL__)
+
+#include <string.h>
+
+#define __set_uint128_t(w1, w0) (((__uint128_t) w1 << 64) | w0)
+
+static const __uint128_t r = __set_uint128_t (0xe100000000000000, 0);
+
+static inline __uint128_t __gfmul (__uint128_t a, __uint128_t b) {
+  __uint128_t z = 0,
+              v = a;
+  for (int i = 0; i < 128; i++) {
+    if ((uint64_t) (b >> (127 - i)) & 1)
+      z = z ^ v;
+    if ((uint64_t) v & 1)
+      v = (v >> 1) ^ r;
+    else
+      v = v >> 1;
+  }
+  return z;
+}
+
+static inline __uint128_t __load_128_t (const uint64_t s[2]) {
+  return __set_uint128_t (be64toh (s[0]), be64toh (s[1]));
+}
+
+static inline __uint128_t __load_128_t_with_padding (const uint8_t *src, size_t n) {
+  uint64_t buf[2] = { 0 };
+  memcpy (buf, src, n);
+  return __load_128_t (buf);
+}
+
+static inline void __store_128_t (uint64_t s[2], __uint128_t x) {
+  s[0] = htobe64 (x >> 64);
+  s[1] = htobe64 (x);
+}
+
+#if defined (__NC_GHASH_LARGE_TABLES)
+#define __t_size   4096
+#define __t_tables 16
+#define __t_width  8
+#define __t_mask   0xff
+#else
+#define __t_size   512
+#define __t_tables 32
+#define __t_width  4
+#define __t_mask   0x0f
+#endif
+
+static inline void __derive (__uint128_t k[__t_size], uint64_t key[2]) {
+  __uint128_t ph, p = (__uint128_t) 1 << 127,
+                  h = __load_128_t (key);
+  for (int i = 0; i < __t_tables; i++) {
+    ph = __gfmul (h, p);
+    for (int j = 0; j < (1 << __t_width); j++)
+      k[(i << __t_width) | j] = __gfmul (ph, (__uint128_t) j << (128 - __t_width));
+    p = p >> __t_width;
+  }
+}
+
+static inline __uint128_t __gfmul_tab (__uint128_t k[__t_size], __uint128_t x) {
+  __uint128_t r = 0;
+  for (int i = 0; i < __t_tables; i++)
+    r ^= k[ ((uint8_t) (x >> ((__t_tables - 1 - i) * __t_width)) & __t_mask)
+            | (i << __t_width) ];
+  return r;
+}
+
+static inline void __ghash (__uint128_t k[__t_size], uint64_t hash[2], const uint8_t *src, size_t n) {
+  __uint128_t acc = __load_128_t (hash);
+  while (n >= 16) {
+    acc = __gfmul_tab (k, acc ^ __load_128_t ((uint64_t *) src));
+    src += 16;
+    n   -= 16;
+  }
+  if (n > 0)
+    acc = __gfmul_tab (k, acc ^ __load_128_t_with_padding (src, n));
+  __store_128_t (hash, acc);
+}
+
+CAMLprim value caml_nc_ghash_key_size (__unit ()) {
+  return Val_int (sizeof (__uint128_t) * __t_size);
+}
+
+CAMLprim value caml_nc_ghash_init_key (value key, value k) {
+  __derive ((__uint128_t *) Bp_val (k), (uint64_t *) Bp_val (key));
+  return Val_unit;
+}
+
+CAMLprim value
+caml_nc_ghash (value k, value hash, value src, value off, value len) {
+  __ghash ((__uint128_t *) Bp_val (k), (uint64_t *) Bp_val (hash),
+           _ba_uint8_off (src, off), Int_val (len) );
+  return Val_unit;
+}
+
+CAMLprim value caml_nc_ghash_mode (__unit ()) { return Val_int (1); }
+
+#endif /* __nc_PCLMUL__ */
