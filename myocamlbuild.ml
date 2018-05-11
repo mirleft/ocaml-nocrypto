6252cc6e34678f54917d03b68887f98c6c50b8b0
Jeremie Dimino <jeremie@dimino.org>
2018-05-11 15:44:47 +0200

    Auto-detect ppx_sexp_conv runtime library

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,24 @@
 open Ocamlbuild_plugin
 
-let () = dispatch Ocb_stubblr.(
-  init & ccopt ~tags:["accelerate"] "-DACCELERATE -mssse3 -maes -mpclmul"
-)
+let runtime_deps_of_ppx ppx =
+  (Findlib.query "ppx_sexp_conv").dependencies
+  |> List.filter_opt (fun { Findlib.name; _ } ->
+      if name = ppx || name = "ppx_deriving" then
+        None
+      else
+        Some name)
+
+let () = dispatch (fun hook ->
+    Ocb_stubblr.(
+      init & ccopt ~tags:["accelerate"] "-DACCELERATE -mssse3 -maes -mpclmul"
+    ) hook;
+    match hook with
+    | After_rules ->
+      let meta = "pkg/META" in
+      let meta_in = meta ^ ".in" in
+      rule meta ~dep:meta_in ~prod:meta (fun _ _ ->
+          let deps = String.concat " " (runtime_deps_of_ppx "ppx_sexp_conv") in
+          Echo([String.subst "PPX_SEXP_CONV_RUNTIME" deps
+                  (Pathname.read meta_in)],
+               meta))
+    | _ -> ())
